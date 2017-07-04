1bb8d0b6fb7befa3a67f77e53bf7ac381f16702a
Rajashekerreddi <Rajashekerreddi@Rajasheker>
2017-07-04 22:48:14 +0530

    first commit

--- src/main/java/MapReduce/Grep/DistributedGrepJob.java
+++ src/main/java/MapReduce/Grep/DistributedGrepJob.java
@@ -1,31 +1,53 @@
 package MapReduce.Grep;
 
+
 import org.apache.hadoop.conf.Configuration;
+
 import org.apache.hadoop.fs.Path;
+
 import org.apache.hadoop.io.NullWritable;
+
 import org.apache.hadoop.io.Text;
+
 import org.apache.hadoop.mapreduce.Job;
+
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+
 import java.io.IOException;
+
 
 /**
  * Created by satheesh on 27/3/17.
  */
-public class DistributedGrepJob {
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        Configuration conf = new Configuration();
-        conf.set("mapregex", "T[0]*");
 
-        Job job = new Job(conf, "Distributed Grep");
-        job.setJarByClass(DistributedGrepJob.class);
-        job.setMapperClass(GrepMapper.class);
-        job.setOutputKeyClass(NullWritable.class);
-        job.setOutputValueClass(Text.class);
+public class DistributedGrepJob {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+     
+   Configuration conf = new Configuration();
+  
+      conf.set("mapregex", "T[0]*");
+
+  
+      Job job = new Job(conf, "Distributed Grep");
+  
+      job.setJarByClass(DistributedGrepJob.class);
+   
+     job.setMapperClass(GrepMapper.class);
+     
+   job.setOutputKeyClass(NullWritable.class);
+    
+    job.setOutputValueClass(Text.class);
+
         job.setNumReduceTasks(0); // Set number of reducers to zero
-        FileInputFormat.addInputPath(job, new Path("/home/satheesh/projects/Java/hadoopAssignments/src/main/java/MapReduce/InvertedIndex/sample-inverted.txt"));
-        FileOutputFormat.setOutputPath(job, new Path("/home/satheesh/projects/Java/hadoopAssignments/src/main/java/MapReduce/Grep/output"));
+  
+      FileInputFormat.addInputPath(job, new Path("/home/satheesh/projects/Java/hadoopAssignments/src/main/java/MapReduce/InvertedIndex/sample-inverted.txt"));
+      FileOutputFormat.setOutputPath(job, new Path("/home/satheesh/projects/Java/hadoopAssignments/src/main/java/MapReduce/Grep/output"));
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
+    
+}
 }
--- src/main/java/MapReduce/Grep/GrepMapper.java
+++ src/main/java/MapReduce/Grep/GrepMapper.java
@@ -1,24 +1,38 @@
 package MapReduce.Grep;
 
+
 import org.apache.hadoop.io.LongWritable;
+
 import org.apache.hadoop.io.NullWritable;
+
 import org.apache.hadoop.io.Text;
+
 import org.apache.hadoop.mapreduce.Mapper;
 
+
 import java.io.IOException;
+
 
 /**
  * Created by satheesh on 27/3/17.
  */
+
 public class GrepMapper extends Mapper<LongWritable, Text, NullWritable, Text> {
+
     public void map(Object key, Text value, Context context)
+
             throws IOException, InterruptedException {
 
-        String txt = value.toString();
-        String mapRegex = "T[0]*";
 
-        if (txt.matches(mapRegex)) {
-            context.write(NullWritable.get(), value);
+        String txt = value.toString();
+ 
+       String mapRegex = "T[0]*";
+
+   
+     if (txt.matches(mapRegex)) {
+    
+        context.write(NullWritable.get(), value);
+
         }
     }
 }
